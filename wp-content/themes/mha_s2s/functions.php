8968002a8b1acfea57fdb8c011956b53f033d3f6
Justin B. Evans <justin@chongandkoster.com>
2021-03-02 18:07:18 -0500

    Performance improvement for chart.js loading.

--- wp-content/themes/mha_s2s/functions.php
+++ wp-content/themes/mha_s2s/functions.php
@@ -89,7 +89,11 @@ function mha_s2s_scripts() {
 	wp_enqueue_script( 'mha_s2s-superfish', get_template_directory_uri() . '/assets/js/superfish.min.js', array(), '1.7.10.1', true );
 	wp_enqueue_script( 'mha_s2s-wow', get_template_directory_uri() . '/assets/js/wow.min.js', array(), '1.3', true );
 	wp_enqueue_script( 'mha_s2s-bootstrap-js', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.bundle.min.js', array(), '4.3.1', true );
-	wp_enqueue_script( 'mha_s2s-chart-js', get_template_directory_uri() . '/assets/js/chart.js', array(), '2.7.2', false );
+	
+	if(get_page_template_slug() == 'templates/page-my-account.php'){
+		wp_enqueue_script( 'mha_s2s-chart-js', get_template_directory_uri() . '/assets/js/chart.js', array(), '2.7.2', false );
+	}
+
 	wp_enqueue_script( 'mha_s2s-sticky', get_template_directory_uri() . '/assets/js/jquery.sticky-kit.js', array(), '1.1.2', true );
 
 	// Load the html5 shiv.
