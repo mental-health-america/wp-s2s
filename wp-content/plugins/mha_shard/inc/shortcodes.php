88b97aaa36e2b3ecf573788bd31c2457d86156f0
Justin B. Evans <justin@chongandkoster.com>
2023-11-29 17:35:18 -0500

    New recurity link shortcodes and popular article fix

--- wp-content/plugins/mha_shard/inc/shortcodes.php
+++ wp-content/plugins/mha_shard/inc/shortcodes.php
@@ -10,6 +10,7 @@ function mha_popular_articles( $options ) {
 	global $wpdb;
 
 	$html = '';
+	$limit = 8;
 	
 	// Default Args
     $defaults = array (
@@ -142,15 +143,19 @@ function mha_popular_articles( $options ) {
 			$html .= '<ol class="plain popular-articles">';
 		}
 		
-		foreach($articles as $a){
-			
+		$counter = 0;
+		foreach($articles as $a){		
+			if($counter >= $limit){
+				break;
+			}	
 			$item_link = '<a class="plain montserrat semi" href="'.get_the_permalink($a).'">'.get_the_title($a).'</a>';
 
 			if($atts['style'] == 'inline'){
 				$inline_list[] = $item_link;
 			} else {
 				$html .= '<li>'.$item_link.'</li>';
-			}			
+			}		
+			$counter++;	
 		}
 
 		if($atts['style'] == 'inline'){
@@ -399,14 +404,16 @@ function mha_display_cta_shortcode( $atts ){
 add_shortcode('mha_cta', 'mha_display_cta_shortcode'); 
 
 
-
+/** 
+ * DIY Tool Display
+ */
 
 /** 
  * DIY Tool Display
  */
 
 function mha_diy_shortcode_display( $atts ){
-
+	$embededed_page = get_the_ID();
     ob_start();
 
     $id = isset($atts['id']) ? intval($atts['id']) : null;
@@ -428,7 +435,8 @@ function mha_diy_shortcode_display( $atts ){
 					case 'question_answer':
 						$template_options = array(
 							'embed' => 1,
-							'embed_type' => $type
+							'embed_type' => $type,
+							'start_page' => $embededed_page
 						);
 						get_template_part( 'templates/diy-tools/question', 'answers', $template_options ); 
 						break;
@@ -441,6 +449,114 @@ function mha_diy_shortcode_display( $atts ){
     } 
 
     return ob_get_clean();
-
 } 
 add_shortcode('mha_diy', 'mha_diy_shortcode_display'); 
+
+
+/** 
+ * Recruit Link Code Generator
+ */
+
+function mha_recruit_link( $args ){
+
+	global $wpdb;
+	
+	// Default Args
+    $defaults = array (
+		'url' => null, 
+		'text' => 'Learn more',
+		'class' => 'button round red'
+	);
+	$atts = wp_parse_args( $args, $defaults );
+	$return = '';
+
+	if($atts['url']){
+
+		$code = $wpdb->get_var("SELECT code FROM cta_codes WHERE used IS NULL LIMIT 1");
+		
+		if($code){
+
+			// Update our record
+			$timezone = new DateTimeZone('America/New_York');
+			$time = new DateTime();
+			$time->setTimezone($timezone);
+			$data = array(
+				'used' 		=> $time->format("Y-m-d H:i:s"),
+				'ipiden' 	=> get_ipiden(),
+				'source' 	=> 'recruit'
+			);
+			$where = array(
+				'code' => $code
+			);
+			$wpdb->update('cta_codes', $data, $where);
+
+			// Return the URL
+			$return = '<a class="'.$atts['class'].'" href="'.add_query_arg( "recruit", $code, $atts["url"] ).'" target="_blank">'.$atts['text'].'</a>';
+			return $return;
+
+		}
+
+	}
+
+	// No codes, return nothing?
+	return false;
+
+} 
+add_shortcode('mha_recruit_link', 'mha_recruit_link'); 
+
+
+/** 
+ * Redcap Survey Link Code Generator
+ */
+
+function mha_redcap_survey_link( $args ){
+
+	global $wpdb;
+	
+	// Default Args
+    $defaults = array (
+		'url' => null, 
+		'text' => 'Pre-fill Survey',
+		'class' => 'button round red'
+	);
+	$atts = wp_parse_args( $args, $defaults );
+	$return = '';
+
+	if($atts['url']){
+
+		$code = $wpdb->get_var("SELECT code FROM cta_codes WHERE used IS NULL LIMIT 1");
+		$random = rand(0,2);
+		
+		if($code){
+
+			// Update our record
+			$timezone = new DateTimeZone('America/New_York');
+			$time = new DateTime();
+			$time->setTimezone($timezone);
+			$data = array(
+				'used' 		=> $time->format("Y-m-d H:i:s"),
+				'ipiden' 	=> get_ipiden(),
+				'source' 	=> 'redcap',
+				'extra' 	=> $random
+			);
+			$where = array(
+				'code' => $code
+			);
+			$wpdb->update('cta_codes', $data, $where);
+
+			// Return the URL
+			$return = '<form method="post" id="redcap-form"  action="'.$atts['url'].'" target="_blank">
+				<input type="hidden" name="usage" id="usage" value="'.$random.'">
+				<input type="hidden" name="n8m95" id="n8m95" value="'.$code.'">
+				<input type="submit" name="__prefill" value="'.$atts['text'].'" class="'.$atts['class'].'"/>
+			</form>';
+			return $return;
+		}
+
+	}
+
+	// No codes, return nothing?
+	return false;
+
+} 
+add_shortcode('mha_redcap_survey_link', 'mha_redcap_survey_link'); 
