084f7df4e6433cf66dd660ef51dcb8f697edf0ee
Justin B. Evans <justin@chongandkoster.com>
2024-05-09 10:35:03 -0400

    Demographic field widths CSS update

--- wp-content/plugins/mha_shard/inc/admin.php
+++ wp-content/plugins/mha_shard/inc/admin.php
@@ -2,7 +2,8 @@
 
 // Custom Admin Javascript
 function mha_admin_scripts($hook) {
-    wp_enqueue_script('my_custom_script', plugin_dir_url(__FILE__) . '/js/mha_admin.js');
+    wp_enqueue_script('mythril_shard_scripts', plugin_dir_url(__FILE__) . 'js/mha_admin.js', array(), '1.0');
+    wp_enqueue_style( 'mythril_shard_scripts', plugin_dir_url(__FILE__) . 'css/mythril_shard.css', array(), '1.1' );
 }
 
 add_action('admin_enqueue_scripts', 'mha_admin_scripts');
