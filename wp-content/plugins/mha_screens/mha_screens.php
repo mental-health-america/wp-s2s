58d1436ebf13a2d10c3271041d3a318bfa89e00c
Justin B. Evans <justin@chongandkoster.com>
2025-12-08 17:11:34 -0500

    [Translation] Header dropdown for languages

--- wp-content/plugins/mha_screens/mha_screens.php
+++ wp-content/plugins/mha_screens/mha_screens.php
@@ -1013,4 +1013,4 @@ include_once 'demographic_steps.php';
 include_once 'featured_next_steps.php';
 include_once 'related_articles.php';
 include_once 'admin_screen_tester.php';
-//include_once 'multilingual.php';
+include_once 'multilingual.php';
